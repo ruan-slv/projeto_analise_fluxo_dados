d9068bdd98d16bfaef46896c2d7c860fa830d05d
Kayogs <kayogabriel.ss16@gmail.com>
2026-08-23 21:17:19 -0300

    feat: criação do ambiente de trabalho do trabalho avaliativo-1 de OLAP e ETL

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+# Criando dados com NumPy
+dados_array = np.array([[10, 20], [30, 40]])
+
+# Criando uma tabela (DataFrame) com Pandas
+df = pd.DataFrame(dados_array, columns=["Coluna A", "Coluna B"])
+
+print("--- Meu primeiro DataFrame com Pandas e NumPy ---")
+print(df)
